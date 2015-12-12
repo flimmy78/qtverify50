31628d5e22a9fa8d25c36578b976b34a0b4b7ddf
songbaoshan <songbaoshan@62fcab80-fd93-7748-8c49-e77283212eb3>
2015-12-12 08:07:08 +0000

    添加qmake生成vs项目的命令脚本， 需设置系统变量%qt50home%
    git-svn-id: https://192.168.1.124:8443/svn/delusoft/trunk/qtverify50@1309 62fcab80-fd93-7748-8c49-e77283212eb3

---
+++ source/makepro.bat
@@ -0,0 +1,20 @@
+cd %qt50home%/source/datatest50/datatestdlg50
+qmake -t vclib
+
+cd %qt50home%/source/flowstandard50
+qmake -t vclib
+
+cd %qt50home%/source/flowweight50
+qmake -t vclib
+
+cd %qt50home%/source/gui50
+qmake -t vcapp
+
+cd %qt50home%/source/totalweight50
+qmake -t vclib
+
+cd %qt50home%/source/waterweight50
+qmake -t vclib
+
+cd %qt50home%/source/systemsetdlg50
+qmake -t vclib
