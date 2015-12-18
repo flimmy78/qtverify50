64fcb887d0eb74f1bb31793777f115a17251ffcc
yangshen <yangshen@62fcab80-fd93-7748-8c49-e77283212eb3>
2015-12-18 07:36:19 +0000

    修改质量法水表检定，在检定流程中增加电动调节阀控制部分，并在界面显示调节阀对应开度等
    git-svn-id: https://192.168.1.124:8443/svn/delusoft/trunk/qtverify50@1341 62fcab80-fd93-7748-8c49-e77283212eb3

--- source/include/waterweight50.h
+++ source/include/waterweight50.h
@@ -135,6 +135,19 @@ public:
 	float m_balMaxWht2;
 	float m_balBottomWht2;
 
+	QTimer *m_regSmallTimer; //计时器，用于动态显示小调节阀的开度
+	int m_smallOpening; 
+	QTimer *m_regMid1Timer; //计时器，用于动态显示中一调节阀的开度
+	int m_mid1Opening; 
+	QTimer *m_regMid2Timer; //计时器，用于动态显示中二调节阀的开度
+	int m_mid2Opening; 
+	QTimer *m_regBigTimer;  //计时器，用于动态显示大调节阀的开度
+	int m_bigOpening; 
+
+	QMap<int, QLineEdit*> m_RegLineEdit; //调节阀端口号与调节阀开度显示控件的映射关系
+	QMap<int, QSpinBox*> m_RegSpinBox; //调节阀端口号与调节阀目标开度控件的映射关系
+	int m_lastPortNO; //记录上一个流量点的阀门端口号（一条管路跑多个流量点时使用）
+
 	void initBalanceCom();     //天平串口
 	void initBalanceCom2();    //天平串口2
 	void initTemperatureCom(); //温度采集串口
@@ -142,6 +155,7 @@ public:
 	void initControlCom2();    //阀门控制串口2
 	void initMeterCom();       //热量表串口
 	void initValveStatus();	   //初始化阀门状态
+	void initRegulateStatus(); //初始化电动调节阀状态
 
 	int isComAndPortNormal();   //串口、端口设置是否正常
 	int isDataCollectNormal();	//检查数据采集是否正常（天平、温度、电磁流量计等）
@@ -245,6 +259,29 @@ public slots:
 	void slotReadNO(const int &row);        //读表号
 
 	void saveStartMeterNO(); //保存起始表号
+
+
+	/*******************电动调节阀******************************/
+	void openAllRegulator();  //打开所有调节阀至设定的开度
+	void closeAllRegulator(); //关闭所有调节阀
+	void setRegulatorOpening(int regNO, int opening); //设置单个调节阀开度，并在界面显示
+
+	void on_btnRegulateSmall_clicked();
+	void on_btnRegulateMid1_clicked();
+	void on_btnRegulateMid2_clicked();
+	void on_btnRegulateBig_clicked();
+	void askControlRegulate(int retNO, int opening); //发送控制调节阀开度命令
+
+	void on_lineEditOpeningSmall_textChanged(const QString & text);
+	void on_lineEditOpeningMid1_textChanged(const QString & text);
+	void on_lineEditOpeningMid2_textChanged(const QString & text);
+	void on_lineEditOpeningBig_textChanged(const QString & text);
+
+	void slotFreshSmallRegOpening();
+	void slotFreshMid1RegOpening();
+	void slotFreshMid2RegOpening();
+	void slotFreshBigRegOpening();
+	/******************电动调节阀end***************************/
 
 private slots:
 
